0c06997be4fe42bf4e27a8e8ced974a2c2e5aee3
camplowell <camplowell@gmail.com>
2023-11-06 21:49:47 -0500

    Condition and blendMatrix nodes

--- core/nodes.py
+++ core/nodes.py
@@ -16,3 +16,13 @@ def matMult(name:str):
 
 def composeMatrix(name:str):
     return cmds.shadingNode('composeMatrix', n=name, au=True)
+
+def condition(name: str):
+    """Returns a Condition node. Default values have been swapped because Maya's are unintuitive."""
+    node = cmds.shadingNode('condition', n=name, au=True)
+    attributes.set_(node, 'colorIfFalse', (0, 0, 0))
+    attributes.set_(node, 'colorIfTrue', (1, 1, 1))
+    return node
+
+def blendMatrix(name: str):
+    return cmds.createNode('blendMatrix', n=name)
